429b94714cd9390f52d36a7675d94be104f0cb0e
FoolOfShadows <foolofshadows@gmail.com>
2018-02-14 10:02:23 -0600

    Initial Commit

---
+++ PTVN Scraper/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  PTVN Scraper
+//
+//  Created by Fool on 2/14/18.
+//  Copyright © 2018 Fulgent Wake. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+	func applicationDidFinishLaunching(_ aNotification: Notification) {
+		// Insert code here to initialize your application
+	}
+
+	func applicationWillTerminate(_ aNotification: Notification) {
+		// Insert code here to tear down your application
+	}
+
+
+}
+
---
+++ PTVN Scraper/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  PTVN Scraper
+//
+//  Created by Fool on 2/14/18.
+//  Copyright © 2018 Fulgent Wake. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+	override func viewDidLoad() {
+		super.viewDidLoad()
+
+		// Do any additional setup after loading the view.
+	}
+
+	override var representedObject: Any? {
+		didSet {
+		// Update the view, if already loaded.
+		}
+	}
+
+
+}
+
